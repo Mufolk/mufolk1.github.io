e78cdb43578a20d429dd6b0cae67a7c50783ce4b
Mufolk <guilhermemelo138@gmail.com>
2017-04-13 23:28:45 -0300

    first commit

---
+++ js/spoiler.js
@@ -0,0 +1,31 @@
+$(document).ready(function() { 
+
+	$("#spoiler-button1").click(function(){
+    	$("#spoiler1").toggle(1000);
+	});
+
+	$("#spoiler-button2").click(function(){
+    	$("#spoiler2").toggle(1000);
+	});
+
+	$("#spoiler-button3").click(function(){
+    	$("#spoiler3").toggle(1000);
+	});
+
+	$("#spoiler-button4").click(function(){
+    	$("#spoiler4").toggle(1000);
+	});
+
+	$("#spoiler-button5").click(function(){
+    	$("#spoiler5").toggle(1000);
+	});
+
+	$("#spoiler-button6").click(function(){
+    	$("#spoiler6").toggle(1000);
+	});
+
+	$("#spoiler-button7").click(function(){
+    	$("#spoiler7").toggle(1000);
+	});
+
+});
